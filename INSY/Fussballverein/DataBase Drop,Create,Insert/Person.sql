90fafd367139837fa4dcb0062a4b2d9b324947bd
Muhi Kanyildiz <Muhi Kanyildiz>
2016-04-07 18:06:26 +0200

    Protokoll.pdf und RM hinzugefügt

--- INSY/Fussballverein/DataBase Drop,Create,Insert/Person.sql
+++ INSY/Fussballverein/DataBase Drop,Create,Insert/Person.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd5d584f19f5014e8b34b71aeec0e225274a37078b72c5df06b62a406810b187
-size 361028673
+oid sha256:ab27cfaaca2dd0a21a126ffabd5a47737a5909477a0ce5c5b0f2225c0df970b1
+size 361028671
