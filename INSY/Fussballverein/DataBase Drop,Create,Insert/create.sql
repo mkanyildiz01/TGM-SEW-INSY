ce5000293fba3722d39cbc9843939bddd862748d
Muhi Kanyildiz <Muhi Kanyildiz>
2016-04-17 17:27:41 +0200

    a

--- INSY/Fussballverein/DataBase Drop,Create,Insert/create.sql
+++ INSY/Fussballverein/DataBase Drop,Create,Insert/create.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea8c6c512df3914b6520f4f0cfdf9ccd1cc14cedd7b472d699ffbff9d87e3892
-size 4528
+oid sha256:5804967cbb37d1716fe7f96eb11c0157288c00ef41dc796da1604784c9dfb4c4
+size 4536
