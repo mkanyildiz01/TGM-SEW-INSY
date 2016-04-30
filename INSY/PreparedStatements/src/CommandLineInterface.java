aeef8f32a698aaa3951d17408a2e1a0ae2d3d207
mkanyildiz01-TGM <mkanyildiz01@student.tgm.ac.at>
2016-04-30 15:21:48 +0200

    PreparedStatements 1. Commit

---
+++ INSY/PreparedStatements/src/CommandLineInterface.java
@@ -0,0 +1,6 @@
+
+public class CommandLineInterface {
+    public static void main(String[]Args){
+
+    }
+}
--- INSY/PreparedStatements/src/PreparedStatements.java
+++ INSY/PreparedStatements/src/PreparedStatements.java
@@ -1,6 +1,3 @@
-/**
- * Created by Muhi_2 on 3/18/2016.
- */
 public class PreparedStatements {
     public static void main(String[]args){
         System.out.println("HalloWelt!");
